5ea6d58bdfb2efb9f34fc7030db4b6fb7565cc3e
krnese <kristiannese@live.com>
2016-01-25 09:44:03 +0100

    added dev PaaS content

---
+++ 04 - Azure Automation and Continious Delivery/Scripts/WebhookExample.ps1
@@ -0,0 +1,27 @@
+﻿# Variables to Set
+$WebhookURI = "https://s2events.azure-automation.net/webhooks?token=TPna6sUVX7y8V2BGA6kgeEh8uYrL0cVer9MIX7lM2GI%3d"
+$VMName = "ContosoVM01"
+
+# Static Variables
+$headers = @{"AuthorizationValue"="Contoso"}
+$WebhookBody  = @([pscustomobject]@{VMName=$VMName})
+
+# Convert to JSON format
+$body = ConvertTo-Json `
+        -InputObject $WebhookBody
+
+# Execute HTTP request
+$response = Invoke-WebRequest `
+            -Method Post `
+            -Uri $WebhookURI `
+            -Headers $headers `
+            -Body $body
+
+# Show response
+$response 
+
+#Get Job ID
+$jobid = (ConvertFrom-Json ($response.Content)).jobids[0]
+
+# Show Job ID information
+$jobid 
