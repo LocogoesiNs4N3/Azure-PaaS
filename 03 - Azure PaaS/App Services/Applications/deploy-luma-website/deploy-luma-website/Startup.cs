5a626ec270b02e0af22f2e2f678c3abff1ff16a3
Ryan Irujo <ryan.irujo@gmail.com>
2016-01-25 11:26:21 +0100

    Added deploy-luma-website application

---
+++ 03 - Azure PaaS/App Services/Applications/deploy-luma-website/deploy-luma-website/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(deploy_luma_website.Startup))]
+namespace deploy_luma_website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ 03 - Azure PaaS/App Services/Applications/deploy-luma-website/deploy-luma-website/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="deploy_luma_website.Global" Language="C#" %>
